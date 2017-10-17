76016a83e1ad5b4d1a61eb458e573dd07f33eb2b
Markus Schulte <markusschulte.work@gmail.com>
2017-10-17 13:51:49 +0200

    Having more Checkstyle errors

--- src/main/java/schulte/markus/App.java
+++ src/main/java/schulte/markus/App.java
@@ -1,11 +1,15 @@
 package schulte.markus;
 
-/**
- * Hello world!
- */
-public class App {
+// Ugly formatting for having Checkstyle-errors.
+public class App
+{
 
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    System.out.println("Hello World1!");
+
+
   }
+
+
+
 }
